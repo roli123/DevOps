4841168ce92997f6f25db4dcb7d8891587a33779
roli123 <rolisingh820@gmail.com>
2025-05-28 16:03:32 +0530

    Data Disk is created and attached to VM

--- Terraform/Azure/VM/vm.tf
+++ Terraform/Azure/VM/vm.tf
@@ -118,3 +118,23 @@ resource "azurerm_windows_virtual_machine" "vm" {
   }
   depends_on = [ azurerm_network_interface.nic, azurerm_resource_group.rg ]
 }
+
+# Block to create additional disk
+
+resource "azurerm_managed_disk" "disk" {
+  name                 = "disk1"
+  location             = local.location
+  resource_group_name  = local.resource_group_name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "10"
+}
+
+# Block to attach disk with VM
+
+resource "azurerm_virtual_machine_data_disk_attachment" "attachdisk" {
+  managed_disk_id    = azurerm_managed_disk.disk.id
+  virtual_machine_id = azurerm_windows_virtual_machine.vm.id
+  lun                = "1"
+  caching            = "ReadWrite"
+}
